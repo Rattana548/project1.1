f4383d84d7867a311766068fde6ae9a9cda893f2
Rattana548 <68465498+Rattana548@users.noreply.github.com>
2020-07-20 18:03:09 +0700

    Add files via upload

---
+++ Project.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+
+	cout << "\n \n   1st Term of 2020                \\451232114       Wanthanee Prachabsupakij \n\n" << endl
+		<< " -----------------------------------------------------------------------------" << endl
+		<< " |   Subject ID   |  Subject name                   |   Score (100)   | Grad |" << endl
+		<< " -----------------------------------------------------------------------------" << endl
+		<< " |    4570034     |  Introduction to compuert       |      85         | \"A\"  |" << endl
+		<< " |    4580247     |  Englisg                        |      74         | \"B\"  |" << endl
+		<< " |    4530001     |  Design Thinking                |      92         | \"A\"  |" << endl
+		<< "-----------------------------------------------------------------------------" << endl
+		<< " |     TOTAL      |  Subject = 3                    |   Average  =  3.87       |" << endl
+		<< " ===============================================================================" << endl 
+		<< endl << endl << endl << endl << " KMUTNB\n\n" << endl;
+
+	system("pause");
+}
